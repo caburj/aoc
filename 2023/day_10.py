bd48750776b502417cda1e23c896b897a1b885cc
Joseph Caburnay <caburnay.joseph@gmail.com>
2023-12-10 19:57:18 +0100

    refactor day 10

--- 2023/day_10.py
+++ 2023/day_10.py
@@ -1,9 +1,23 @@
 from utils import get_test_input, get_input
+from itertools import product
 
-# coordinates are (row, col)
+# Pipe coordinates are (row_index, column_index).
+
+
+N = "|F7"
+E = "-7J"
+W = "-LF"
+S = "|LJ"
+
+NS = set(product(N, S))
+WE = set(product(W, E))
+NE = set(product(N, E))
+NW = set(product(N, W))
+SE = set(product(S, E))
+SW = set(product(S, W))
+
 
 connections = {
-    ".": [],
     "|": [(-1, 0), (1, 0)],
     "-": [(0, -1), (0, 1)],
     "L": [(-1, 0), (0, 1)],
@@ -15,25 +29,17 @@ connections = {
 
 def identify_start(neighbors):
     n, e, w, s = neighbors
-    if n == "|" and s == "|":
+    if (n, s) in NS:
         return "|"
-    elif e == "-" and w == "-":
+    elif (w, e) in WE:
         return "-"
-    elif n == "|" and e == "-":
+    elif (n, e) in NE:
         return "L"
-    elif n == "|" and w == "-":
+    elif (n, w) in NW:
         return "J"
-    elif s == "|" and e == "-":
+    elif (s, e) in SE:
         return "F"
-    elif s == "|" and w == "-":
-        return "7"
-    elif e == "J" and s == "|":
-        return "F"
-    elif w == "L" and n == "F":
-        return "J"
-    elif e == "7" and s == "J":
-        return "F"
-    elif w == "F" and s == "|":
+    elif (s, w) in SW:
         return "7"
     else:
         raise ValueError("start is not connected")
@@ -50,222 +56,106 @@ def parse_input(input):
     for line in lines:
         grid.append("." + line + ".")
     grid.append("." * (width + 2))
-    return grid, (1, 1), (height, width)
+
+    topleft = (1, 1)
+    bottomright = (height, width)
+
+    sr, sc = find_start(grid, topleft, bottomright)
+    neighbors = (grid[sr + r][sc + c] for r, c in [(-1, 0), (0, 1), (0, -1), (1, 0)])
+    start_id = identify_start(neighbors)
+
+    grid[sr] = grid[sr].replace("S", start_id)
+
+    return grid, (sr, sc), topleft, bottomright
 
 
-def find_start(grid, topleft, bottomright, start_char):
-    sr, sc = topleft
-    er, ec = bottomright
-    for row in range(sr, er + 1):
-        for col in range(sc, ec + 1):
-            if grid[row][col] == start_char:
+def find_start(grid, topleft, bottomright):
+    start_r, start_c = topleft
+    end_r, end_c = bottomright
+    for row in range(start_r, end_r + 1):
+        for col in range(start_c, end_c + 1):
+            if grid[row][col] == "S":
                 return row, col
 
 
-def get_all_neighbors(grid, row, col):
-    return (grid[row + r][col + c] for r, c in [(-1, 0), (0, 1), (0, -1), (1, 0)])
-
-
-def get_linked_parts(g, r, c, part, start):
-    sr, sc, sp = start
-    return [
-        (r + i, c + j, sp if r + i == sr and c + j == sc else g[r + i][c + j])
-        for i, j in connections[part]
-    ]
+def get_linked_parts(g, r, c):
+    return [(r + i, c + j) for i, j in connections.get(g[r][c], [])]
 
 
 def get_next(links, prev):
     return next(link for link in links if link != prev)
 
 
-def find_loop(g, tl, br):
-    start = find_start(g, tl, br, "S")
-    neighbors = get_all_neighbors(g, *start)
-    start_part = identify_start(neighbors)
-    _start = (*start, start_part)
-    loop = [_start]
-    prev = _start
-    current = get_linked_parts(g, *_start, _start)[1]
-    while current != _start:
+def find_loop(g, s):
+    loop = [s]
+    prev = s
+    current = get_linked_parts(g, *s)[1]
+    while current != s:
         temp = current
         loop.append(current)
-        current = get_next(get_linked_parts(g, *current, _start), prev)
+        current = get_next(get_linked_parts(g, *current), prev)
         prev = temp
-
     return loop
 
 
-def num_enclosed(g, tl, br):
-    loop = find_loop(g, tl, br)
-    set_loop = set(loop)
+def num_enclosed(g, s, tl, br):
+    loop = find_loop(g, s)
+    loop_set = set(loop)
     count = 0
     for i in range(tl[0], br[0] + 1):
         for j in range(tl[1], br[1] + 1):
-            if g[i][j] == "S" or (i, j, g[i][j]) in set_loop:
+            if (i, j) in loop_set:
                 continue
 
-            rights = chi(sorted([(r, c, p) for r, c, p in loop if c > j and r == i], key=lambda x: x[1]), i, j)
-            lefts = chi(sorted([(r, c, p) for r, c, p in loop if c < j and r == i], key=lambda x: -x[1]), i, j)
-            ups = cvi(sorted([(r, c, p) for r, c, p in loop if r < i and c == j], key=lambda x: -x[0]), i, j)
-            downs = cvi(sorted([(r, c, p) for r, c, p in loop if r > i and c == j], key=lambda x: x[0]), i, j)
+            horizontal = sorted([(r, c) for r, c in loop if r == i], key=lambda x: x[1])
+            vertical = sorted([(r, c) for r, c in loop if c == j], key=lambda x: x[0])
 
-            if any(x == 0 for x in [rights, lefts, ups, downs]):
+            r_pipes = [(r, c) for r, c in horizontal if c > j]
+            l_pipes = [(r, c) for r, c in horizontal if c < j]
+            t_pipes = [(r, c) for r, c in vertical if r < i]
+            b_pipes = [(r, c) for r, c in vertical if r > i]
+
+            n_intersections = [
+                count_intersections(g, r_pipes, "-", "|"),
+                count_intersections(g, l_pipes, "-", "|"),
+                count_intersections(g, t_pipes, "|", "-"),
+                count_intersections(g, b_pipes, "|", "-"),
+            ]
+
+            if not all(n_intersections):
                 continue
 
-            if any(x % 2 == 1 for x in [rights, lefts, ups, downs]):
+            if any(x % 2 == 1 for x in n_intersections):
                 count += 1
 
     return count
 
 
-def cvi(items, i, j):
-    # Count Vertical Intersections
+def count_intersections(g, items, to_ignore, to_count):
     n = 0
     ignore = False
     prev_ignored = None
-    for r, c, p in items:
-        if p == "-":
+    for r, c in items:
+        p = g[r][c]
+        if p == to_count:
             n += 1
             continue
 
-        if ignore and p == "|":
+        if ignore and p == to_ignore:
             continue
 
-        if p == "F":
-            if ignore:
-                if prev_ignored == "J":
-                    n += 1
-                elif prev_ignored == "L":
-                    n += 0
-                else:
-                    print(i, j, r, c, p, prev_ignored)
-
-                ignore = False
+        match p, ignore, prev_ignored:
+            case ("F", True, "J") | ("J", True, "F") | ("L", True, "7") | (
+                "7",
+                True,
+                "L",
+            ):
+                n += 1
                 prev_ignored = None
-            else:
-                ignore = True
-                prev_ignored = "F"
+            case p, False, _:
+                prev_ignored = p
 
-        elif p == "L":
-            if ignore:
-                if prev_ignored == "7":
-                    n += 1
-                elif prev_ignored == "F":
-                    n += 0
-                else:
-                    print(i, j, r, c, p, prev_ignored)
-
-                ignore = False
-                prev_ignored = None
-            else:
-                ignore = True
-                prev_ignored = "L"
-
-        elif p == "7":
-            if ignore:
-                if prev_ignored == "L":
-                    n += 1
-                elif prev_ignored == "J":
-                    n += 0
-                else:
-                    print(i, j, r, c, p, prev_ignored)
-
-                ignore = False
-                prev_ignored = None
-            else:
-                ignore = True
-                prev_ignored = "7"
-
-        elif p == "J":
-            if ignore:
-                if prev_ignored == "F":
-                    n += 1
-                elif prev_ignored == "7":
-                    n += 0
-                else:
-                    print(i, j, r, c, p, prev_ignored)
-
-                ignore = False
-                prev_ignored = None
-            else:
-                ignore = True
-                prev_ignored = "J"
-
-    return n
-
-
-def chi(items, i, j):
-    # Count Horizontal Intersections
-    n = 0
-    ignore = False
-    prev_ignored = None
-    for r, c, p in items:
-        if p == "|":
-            n += 1
-            continue
-
-        if ignore and p == "-":
-            continue
-
-        if p == "F":
-            if ignore:
-                if prev_ignored == "J":
-                    n += 1
-                elif prev_ignored == "7":
-                    n += 0
-                else:
-                    print(i, j, r, c, p, prev_ignored)
-
-                ignore = False
-                prev_ignored = None
-            else:
-                ignore = True
-                prev_ignored = "F"
-
-        elif p == "L":
-            if ignore:
-                if prev_ignored == "7":
-                    n += 1
-                elif prev_ignored == "J":
-                    n += 0
-                else:
-                    print(i, j, r, c, p, prev_ignored)
-
-                ignore = False
-                prev_ignored = None
-            else:
-                ignore = True
-                prev_ignored = "L"
-
-        elif p == "7":
-            if ignore:
-                if prev_ignored == "L":
-                    n += 1
-                elif prev_ignored == "F":
-                    n += 0
-                else:
-                    print(i, j, r, c, p, prev_ignored)
-
-                ignore = False
-                prev_ignored = None
-            else:
-                ignore = True
-                prev_ignored = "7"
-
-        elif p == "J":
-            if ignore:
-                if prev_ignored == "F":
-                    n += 1
-                elif prev_ignored == "L":
-                    n += 0
-                else:
-                    print(i, j, r, c, p, prev_ignored)
-
-                ignore = False
-                prev_ignored = None
-            else:
-                ignore = True
-                prev_ignored = "J"
+        ignore = not ignore
 
     return n
 
@@ -273,11 +163,14 @@ def chi(items, i, j):
 if __name__ == "__main__":
     test_input = get_test_input(__file__)
     test_input_2 = get_test_input(__file__, "2")
-
     input = get_input(__file__)
-    assert len(find_loop(*parse_input(test_input))) // 2 == 4
-    assert len(find_loop(*parse_input(test_input_2))) // 2 == 8
-    print("Part 1:", len(find_loop(*parse_input(input))) // 2)
+
+    g1, s1, *_ = parse_input(test_input)
+    g2, s2, *_ = parse_input(test_input_2)
+    g, s, tl, br = parse_input(input)
+    assert len(find_loop(g1, s1)) // 2 == 4
+    assert len(find_loop(g2, s2)) // 2 == 8
+    print("Part 1:", len(find_loop(g, s)) // 2)
 
     test_input_3 = get_test_input(__file__, "3")
     test_input_4 = get_test_input(__file__, "4")
@@ -288,4 +181,4 @@ if __name__ == "__main__":
     assert num_enclosed(*parse_input(test_input_4)) == 4
     assert num_enclosed(*parse_input(test_input_5)) == 8
     assert num_enclosed(*parse_input(test_input_6)) == 10
-    print("Part 2:", num_enclosed(*parse_input(input)))
+    print("Part 2:", num_enclosed(g, s, tl, br))
