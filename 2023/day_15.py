0a3c2bac01b8330572764e8af1726058794c5d21
Joseph Caburnay <caburnay.joseph@gmail.com>
2023-12-16 16:55:32 +0100

    refactor: day 15

--- 2023/day_15.py
+++ 2023/day_15.py
@@ -1,45 +1,33 @@
+import re
+from functools import reduce
 from utils import get_test_input, get_input, run
 
 
 def hash(s):
-    h = 0
-    for c in s:
-        h += ord(c)
-        h *= 17
-        h %= 256
-    return h
+    return reduce(lambda h, c: (h + ord(c)) * 17 % 256, s, 0)
 
 
 def parse_commands(ss):
-    commands = []
-    for s in ss:
-        if "=" in s:
-            box_hash, payload = s.split("=")
-            commands.append((box_hash, "=", int(payload)))
-        else:
-            box_hash, _ = s.split("-")
-            commands.append((box_hash, "-", None))
-    return commands
+    return [re.compile(r"[-=]").split(s) for s in ss]
 
 
 def run_commands(commands):
     boxes = [dict() for _ in range(256)]
     for command in commands:
-        box_hash, op, payload = command
-        box_id = hash(box_hash)
-        lenses = boxes[box_id]
-        if op == "-":
-            if box_hash in lenses:
-                del lenses[box_hash]
+        box, payload = command
+        lenses = boxes[hash(box)]
+        if payload == "":
+            if box in lenses:
+                del lenses[box]
         else:
-            lenses[box_hash] = payload
+            lenses[box] = int(payload)
     return boxes
 
 
 def get_focusing_power(boxes):
     result = 0
     for box_id, lenses in enumerate(boxes):
-        for slot_id, (_box_hash, power) in enumerate(lenses.items()):
+        for slot_id, power in enumerate(lenses.values()):
             result += (box_id + 1) * (slot_id + 1) * power
     return result
 
