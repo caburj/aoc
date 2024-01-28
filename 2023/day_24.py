e72e50a44bfe57a93b47d52f38d0299d735d2070
Joseph Caburnay <caburnay.joseph@gmail.com>
2024-01-28 12:59:32 +0100

    day 24 part 2

--- 2023/day_24.py
+++ 2023/day_24.py
@@ -2,6 +2,8 @@ from fractions import Fraction
 from itertools import combinations
 from utils import get_input, get_test_input, run
 
+import z3
+
 
 def parse_line(line):
     return tuple(map(parse_coords, line.strip().split("@")))
@@ -77,22 +79,44 @@ def count_intersections(lines, is_included):
     return future_intersections
 
 
+def find_magical_stone(hailstones):
+    x, y, z, dx, dy, dz = z3.Ints("x y z dx dy dz")
+    s = z3.Solver()
+    for i, ((hx, hy, hz), (hdx, hdy, hdz)) in enumerate(hailstones):
+        # need each hailstone to be hit by the "lazer" (magical stone)
+        ti = z3.Int(f"t_{i}")
+        s.add(ti > 0)
+        s.add(x + dx * ti == hx + hdx * ti)
+        s.add(y + dy * ti == hy + hdy * ti)
+        s.add(z + dz * ti == hz + hdz * ti)
+
+    if s.check() == z3.sat:
+        m = s.model()
+        x_val = m[x].as_long()
+        y_val = m[y].as_long()
+        z_val = m[z].as_long()
+        return x_val + y_val + z_val
+
+
 def test():
     input = get_test_input(__file__)
     lines = list(map(get_2d_line, *zip(*parse_input(input))))
     assert count_intersections(lines, lambda x, y: x > 7 and y < 27) == 2
+    assert find_magical_stone(parse_input(input)) == 47
 
 
 def main():
     input = get_input(__file__)
     lines = list(map(get_2d_line, *zip(*parse_input(input))))
     print(
+        "Part 1:",
         count_intersections(
             lines,
             lambda x, y: 200000000000000 <= x <= 400000000000000
             and 200000000000000 <= y <= 400000000000000,
-        )
+        ),
     )
+    print("Part 2:", find_magical_stone(parse_input(input)))
 
 
 run(main, test, ignore_other_exceptions=False)
